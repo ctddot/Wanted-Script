1f172fe4c215f0943ac406ef58097a2814522e74
ctddot <ctddot@gmail.com>
2026-06-02 18:08:57 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/vehicle.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ctddot/Wanted-Script/main/vehicle.lua"))()
